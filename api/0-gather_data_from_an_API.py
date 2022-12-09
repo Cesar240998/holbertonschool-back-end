dbc9940cae38dc8b740449082350d187c8ac5bec
Cesar240998 <cesar.evanan@utec.edu.pe>
2022-12-08 22:46:18 -0500

    fix code

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -7,34 +7,38 @@ from requests import get
 from sys import argv
 
 
-def information_employee():
-    ''' Gather data from an API '''
-    emp_id = int(argv[1])
-    emp_name = ''
-    tasks_done = 0
-    tasks_total = 0
-    tasks_titles = []
+if __name__ == "__main__":
+    # url base
+    url_base = 'https://jsonplaceholder.typicode.com/'
+    user_id = int(argv[1])
+    EMPLOYEE_NAME = ""
+    msg = 'Employee {} is done with tasks({}/{}):'
+    # url (all_user, id_user, todos, cmp_task)
+    url_user = '{}users'.format(url_base)
+    url_id = '{}{}'.format(url_user, user_id)
+    url_todos = '{}todos'.format(url_base)
+    url_cmp_tk = '{}?completed=true'.format(url_todos)
+    response_user = requests.get(url_user)
+    user_json = response_user.json()
 
-    users_res = get('https://jsonplaceholder.typicode.com/users').json()
-    for user in users_res:
-        if user['id'] == emp_id:
-            emp_name = user['name']
-            break
+    for i in user_json:
+        if i.get("id") == user_id:
+            EMPLOYEE_NAME = i.get("name")
+    response_todos = requests.get(url_todos)
+    todos_json = response_todos.json()
+    DONE_TASKS = []
+    for i in todos_json:
+        if i.get("userId") == user_id and i.get("completed") is True:
+            DONE_TASKS.append(i.get("title"))
+    NUMBER_OF_DONE_TASKS = len(DONE_TASKS)
+    TOTAL_NUMBER_OF_TASK = 0
 
-    tasks_res = get('https://jsonplaceholder.typicode.com/todos').json()
-    for task in tasks_res:
-        if task['userId'] == emp_id:
-            if task['completed']:
-                tasks_titles.append(task['title'])
-                tasks_done += 1
-            tasks_total += 1
-
-    print('Employee {} is done with tasks({}/{}):'.format(emp_name,
-                                                          tasks_done,
-                                                          tasks_total))
-    for title in tasks_titles:
-        print('\t {}'.format(title))
-
-
-if __name__ == '__main__':
-    information_employee()
+    for i in todos_json:
+        if i.get("userId") == user_id:
+            TOTAL_NUMBER_OF_TASK += 1
+    print("Employee {} is done with tasks({}/{}):"
+          .format(EMPLOYEE_NAME,
+                  NUMBER_OF_DONE_TASKS,
+                  TOTAL_NUMBER_OF_TASK))
+    for i in DONE_TASKS:
+        print("\t {}".format(i))
